172157ca746e56ee885c2b06b3cbbe3d0629e3b5
Siddarth Kaki <siddarthkaki@utexas.edu>
2017-11-16 16:41:53 -0600

    Enforce C++11 with compiler flag

--- compile.sh
+++ compile.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
-g++ obj_localiser.cpp -o obj_localiser $(pkg-config --libs opencv --cflags)
+g++ obj_localiser.cpp -o obj_localiser -std=c++11 $(pkg-config --libs opencv --cflags)
 exit 0
