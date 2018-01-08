5a677358e2718d8317dfc6ee93da3f42b0346417
Siddarth Kaki <siddarthkaki@utexas.edu>
2018-01-08 12:07:51 -0600

    Added timing and debug outputs

--- project_points/cpp/main.cpp
+++ project_points/cpp/main.cpp
@@ -5,6 +5,7 @@
  */
 
 #include <stdio.h>
+#include <time.h>
 #include <fstream>
 #include <iterator>
 #include <iostream>
@@ -32,6 +33,10 @@ int main( int argc, char** argv )
     if( argc != 3 )
     { readme(); return -1; }
 
+    // start clock
+    clock_t t;
+    t = clock();
+
     // read in points3D.txt file
     std::string points_file_id = argv[1];
 
@@ -43,8 +48,8 @@ int main( int argc, char** argv )
     char *zErrMsg = 0;
     int rc;
     rc = sqlite3_open(db_file_id.c_str(), &db);
-    if( rc ) { fprintf(stderr, " Can't open database: %s\n", sqlite3_errmsg(db)); return(0); }
-    else     { fprintf(stderr, " Opened database successfully\n"); }
+    if( rc ) { fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db)); return(0); }
+    else     { fprintf(stderr, "Opened database successfully\n"); }
     //sqlite3_close(db);
 
     unsigned char *pzBlob; // holder pointer to blob
@@ -53,7 +58,7 @@ int main( int argc, char** argv )
     // number of 3D points in file
     unsigned num_points = LineCount(points_file_id) - 3;
 
-    printf(" Num 3D points: %u\n", num_points);
+    printf("Num 3D points: %u\n", num_points);
 
     MatrixXd pos_mat(num_points,3); // matrix of 3D position for each point
     MatrixXi mean_descriptors(num_points,128); // matrix of mean 128-length SIFT descriptors for each point
@@ -68,9 +73,10 @@ int main( int argc, char** argv )
         for( unsigned i = 0; i < 3; i++ ) { getline( input_file, curr_line ); }
 
         // loop through each point in 3D point cloud
-        for( unsigned i = 0; i < 1; i++ )
+        for( unsigned i = 0; i < num_points; i++ )
         {
-            if (i % 1000 == 0) { std::cout << i << std::endl; }
+            //if (i % 1000 == 0) { std::cout << i << std::endl; }
+            //if (i % 9213 == 0) { std::cout << "Here" << std::endl; }
 
             getline( input_file, curr_line );
 
@@ -90,21 +96,24 @@ int main( int argc, char** argv )
             pos_mat(i,1) = tokenised_double.at(2); // Y pos
             pos_mat(i,2) = tokenised_double.at(3); // Z pos
 
+            //if (i % 9213 == 0) { std::cout << "Here" << std::endl; }
+
             unsigned num_tracks = (tokenised_double.size() - 8)/2; // number of images with this point
 
-            std::cout << "Num Images: " << num_tracks << std::endl << std::endl;
+            //if (i % 1000 == 0) { std::cout << "Num Images: " << num_tracks << std::endl << std::endl; }
     
             MatrixXi curr_descriptors(num_tracks,128); // matrix of descriptors for each point
 
             // loop through each image with this point
             for( unsigned j = 0; j < num_tracks; j++ )
             {
-                std::cout << "Image: " << j+1 << std::endl;
+                //if (i % 9213 == 0) { std::cout << "Here: j = " << j << std::endl; }
+                //if (i % 1000 == 0) { std::cout << "Image: " << j+1 << std::endl; }
                 unsigned curr_image_id = tokenised_double.at(6+2*(j+1)); // IMAGE_ID
                 unsigned curr_point2d_idx = tokenised_double.at(7+2*(j+1)); // POINT2D_IDX
 
-                std::cout << "Current IMAGE_ID: " << curr_image_id << std::endl;
-                std::cout << "Current POINT2D_IDX: " << curr_point2d_idx << std::endl;
+                //if (i % 9213 == 0) { std::cout << "Current IMAGE_ID: " << curr_image_id << std::endl; }
+                //if (i % 9213 == 0) { std::cout << "Current POINT2D_IDX: " << curr_point2d_idx << std::endl; }
 
                 sqlite3_stmt *stmt;
                 std::string zSql = "SELECT * FROM descriptors WHERE image_id=" + std::to_string(curr_image_id);
@@ -119,23 +128,24 @@ int main( int argc, char** argv )
                     std::vector<char> data( sqlite3_column_bytes(stmt, 3) );
                     const char *pBuffer = reinterpret_cast<const char*>( sqlite3_column_blob(stmt, 3) );
                     std::copy( pBuffer, pBuffer + data.size(), &data[0] );
-                                      
+
                     unsigned curr_start = (curr_point2d_idx-1)*128; // checked
                     unsigned curr_end = curr_start+127; // checked
 
-                    std::cout << "BLOB Start Index: " << curr_start << std::endl;
+                    //if (i % 9213 == 0) { std::cout << "BLOB Start Index: " << curr_start << std::endl; }
                     
-                    std::cout << "BLOB Size: " << data.size() << std::endl
-                              << "Num Descriptors: " << data.size()/128 << std::endl;
+                    //if (i % 9213 == 0) { std::cout << "BLOB Size: " << data.size() << std::endl
+                    //                               << "Num Descriptors: " << data.size()/128 << std::endl; }
 
                     VectorXi curr_descriptor(128);
 
                     for ( unsigned k = 0; k < 128; k++ )
                     {
+                        //if (i % 9213 == 0) { std::cout << "Here: k = " << k << std::endl; }
                         curr_descriptor(k) = ( (int) ((uint8_t) data.at(curr_start + k)) );
-                        std::cout << curr_descriptor(k) << ' ';
+                        //if (i % 1000 == 0) { std::cout << curr_descriptor(k) << ' '; }
                     }
-                    std::cout << std::endl << std::endl;
+                    //if (i % 1000 == 0) { std::cout << std::endl << std::endl; }
 
                     curr_descriptors.block<1,128>(j,0) = curr_descriptor;
                 }
@@ -147,7 +157,8 @@ int main( int argc, char** argv )
             VectorXi mean_descriptor(128);
             mean_descriptor = curr_descriptors.colwise().mean();
             mean_descriptors.block<1,128>(i,0) = mean_descriptor;
-            std::cout << mean_descriptor.transpose() << std::endl;
+            //if (i % 1000 == 0) { std::cout << mean_descriptor.transpose() << std::endl; }
+            if (i % 1000 == 0) { std::cout << "Completed point: " << i << std::endl; }
         }
 
         //std::cout << mean_descriptors << std::endl;
@@ -157,6 +168,10 @@ int main( int argc, char** argv )
 
     //std::cout << pos_mat << std::endl;
     //waitKey(0);
+
+    // stop clock
+    t = clock() - t;
+    printf ("Run time: %f seconds\n",((float)t)/CLOCKS_PER_SEC);
 
     return 0;
 }
