bda0917575686b1c00e701b7ac1012c5f11015fc
Siddarth Kaki <siddarthkaki@utexas.edu>
2018-02-13 10:33:20 -0600

    Added keypoint extraction from db; keypoint locations mismatch though

--- main.cpp
+++ main.cpp
@@ -37,11 +37,17 @@ using Eigen::VectorXi;
 using Eigen::Vector3d;
 
 void readme();
+
 std::map<std::string,std::string> LoadConfig(std::string filename);
+
 Rect BoundingBox(Mat *img_cam, std::vector<KeyPoint> *keypoints);
+
 std::vector< std::vector<KeyPoint> > DBSCAN_keypoints(std::vector<KeyPoint> *keypoints, float eps, int min_pts);
 std::vector<int> RegionQuery(std::vector<KeyPoint> *keypoints, KeyPoint *keypoint, float eps);
-cv::Mat ColmapSiftFeatures(std::map<std::string,std::string> config_params);
+
+void ColmapSiftFeatures(std::map<std::string,std::string> config_params, std::string cm_database_path);
+std::vector<cv::KeyPoint> ColmapSiftKeypoints(std::string cm_database_path);
+cv::Mat ColmapSiftDescriptors(std::string cm_database_path);
 
 /**
  * @function main
@@ -169,15 +175,18 @@ int main( int argc, char** argv )
 
     SiftFeatureDetector detector( min_hessian );
     // TODO - check if keypoints_map is actually needed
-    std::vector<cv::KeyPoint> keypoints_cam, keypoints_map;
-    detector.detect( img_cam, keypoints_cam );
+    //std::vector<cv::KeyPoint> keypoints_cam, keypoints_map;
+    //detector.detect( img_cam, keypoints_cam );
 
     //-- Step 2: Calculate descriptors (feature vectors)
     SiftDescriptorExtractor extractor;
     //cv::Mat descriptors_cam;
     //extractor.compute( img_cam, keypoints_cam, descriptors_cam );
 
-    cv::Mat descriptors_cam = ColmapSiftFeatures(config_params);
+    std::string cm_database_path = "brickseal1_img_cam.db";
+    ColmapSiftFeatures(config_params, cm_database_path);
+    std::vector<cv::KeyPoint> keypoints_cam = ColmapSiftKeypoints(cm_database_path);
+    cv::Mat descriptors_cam = ColmapSiftDescriptors(cm_database_path);
 
     // convert Eigen matrix of mean descriptors to CV matrix
     cv::Mat descriptors_map;
@@ -492,20 +501,82 @@ vector<int> RegionQuery(std::vector<KeyPoint> *keypoints, KeyPoint *keypoint, fl
 
 /**
  * @function ColmapSiftFeatures
- * @brief VLFeat implementation of SIFT feature extraction; from COLMAP; todo citation
+ * @brief COLMAP implementation of SIFT feature extraction
  */
-cv::Mat ColmapSiftFeatures(std::map<std::string,std::string> config_params)
+void ColmapSiftFeatures(std::map<std::string,std::string> config_params, std::string cm_database_path)
 {
     // COLMAP SYS CALL
     std::string cm_image_path = config_params.at("img_cam");
     system("mkdir .temp_img");
     std::string sys_call = "cp " + cm_image_path + " .temp_img/";
     system(sys_call.c_str());
-    std::string cm_database_path = "brickseal1_img_cam.db";
+    //std::string cm_database_path = "brickseal1_img_cam.db";
     //std::cout << cm_database_path << std::endl;
     std::string cm_sys_call = "colmap feature_extractor --database_path " + cm_database_path + " --image_path .temp_img --SiftExtraction.use_gpu 0";
     std::cout << cm_sys_call << std::endl;
     int cm_sys_res = system(cm_sys_call.c_str());
+}
+
+/**
+ * @function ColmapSiftKeypoints
+ * @brief COLMAP implementation of SIFT keypoints
+ */
+std::vector<cv::KeyPoint> ColmapSiftKeypoints(std::string cm_database_path)
+{
+    // setup db connection
+    sqlite3 *db;
+    char *zErrMsg = 0;
+    int rc;
+    rc = sqlite3_open(cm_database_path.c_str(), &db);
+    if( rc ) { fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db)); exit (EXIT_FAILURE); }
+    else     { fprintf(stderr, "Opened database successfully\n"); }
+
+    // keypoint extraction ////////////////////////////////////////////////////
+    //unsigned char *pzBlob; // holder pointer to blob
+    //int *pnBlob; // retrieved blob size
+
+    sqlite3_stmt *stmt;
+    std::string zSql = "SELECT * FROM keypoints";
+    sqlite3_prepare_v2(db, zSql.c_str(), -1, &stmt, NULL);
+    rc = sqlite3_step(stmt);
+
+    std::vector<cv::KeyPoint> img_keypoints;
+
+    if (rc == SQLITE_ROW)
+    {
+        unsigned num_keypoints = sqlite3_column_int(stmt,1); // get num of descriptors from column 1
+        unsigned num_cols = sqlite3_column_int(stmt,2); // get num of descriptors from column 1
+
+        // allocate space for entire descriptors matrix
+        //img_descriptors.resize(curr_num_descriptors,128);
+
+        // get float32 BLOB data from db
+        std::vector<char> data( sqlite3_column_bytes(stmt, 3) );
+        const char *pBuffer = reinterpret_cast<const char*>( sqlite3_column_blob(stmt, 3) );
+        std::copy( pBuffer, pBuffer + data.size(), &data[0] );
+
+        unsigned curr_start = 0; // checked
+
+        for ( unsigned j = 0; j < num_keypoints; j++ )
+        {
+            cv::KeyPoint kp_temp;
+            kp_temp.pt.x = (float) ((float) data.at(curr_start + 0));
+            kp_temp.pt.y = (float) ((float) data.at(curr_start + 1));
+
+            img_keypoints.push_back(kp_temp);
+
+            curr_start += 6;
+        }
+    }
+    return img_keypoints;
+}
+
+/**
+ * @function ColmapSiftDescriptors
+ * @brief COLMAP implementation of SIFT descriptors
+ */
+cv::Mat ColmapSiftDescriptors(std::string cm_database_path)
+{
 
     // setup db connection
     sqlite3 *db;
@@ -515,8 +586,7 @@ cv::Mat ColmapSiftFeatures(std::map<std::string,std::string> config_params)
     if( rc ) { fprintf(stderr, "Can't open database: %s\n", sqlite3_errmsg(db)); exit (EXIT_FAILURE); }
     else     { fprintf(stderr, "Opened database successfully\n"); }
 
-    unsigned char *pzBlob; // holder pointer to blob
-    int *pnBlob; // retrieved blob size
+    // descriptor extraction //////////////////////////////////////////////////
 
     sqlite3_stmt *stmt;
     std::string zSql = "SELECT * FROM descriptors";
